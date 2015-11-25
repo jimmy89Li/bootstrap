6be988ae52a7f386ed0ffed686f945698813288d
jimmyLi <liviu.c.simion@gmail.com>
2015-11-25 14:01:11 +0100

    added login, profile page and blog roll

---
+++ login_code.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$dbi = mysqli_connect('localhost','root','','marketguide_user') or die(mysql_errno());
+   
+if ($dbi->connect_error ){
+    die('kan ikke forbinde til SQLserver' . $dbi->connect_errno .
+    ":".$dbi->connect_error);
+}
+$dbi->set_charset('utf8');
+  
+   session_start();
+
+  /*if (isset($_POST['login'])) {*/
+      
+
+              $sql = "SELECT * FROM user WHERE email = '" . $_POST['email'] . "' && password = '" . $_POST['password'] . "'";
+             
+              $result = $dbi->query($sql) or die(error_reporting());
+
+                 if ($result->num_rows == 1) {
+                   $_SESSION["login"] = 'true';
+
+                   
+                   
+
+                  
+
+                  
+                 header("location:profile.php");
+                      
+                      
+                 } else {
+                     /* .. DU ER IKKE LOGGET IND .. */
+                       $_SESSION['login'] = 'false';
+                     header("location:index.php");
+                     
+             }
+            
+?>
